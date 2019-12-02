4d285bf921905705ceaa1b0b135068258ac880e5
ned1313 <ned.bellavance@gmail.com>
2019-12-02 07:00:44 -0500

    add manual step

--- deploy-aks/aadpodidentity.sh
+++ deploy-aks/aadpodidentity.sh
@@ -8,3 +8,8 @@ kubectl apply -f https://raw.githubusercontent.com/Azure/aad-pod-identity/master
 #Install and bind the Azure Identity
 kubectl apply -f ./yaml_files/aadpodidentity.yaml
 kubectl apply -f ./yaml_files/aadpodidentitybinding.yaml
+
+
+response=$(curl 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -H Metadata:true -s)
+access_token=$(echo $response | python -c 'import sys, json; print (json.load(sys.stdin)["access_token"])')
+curl https://<VAULT_NAME>.vault.azure.net/secrets/everything?api-version=2016-10-01 -H "Authorization: Bearer $access_token"
